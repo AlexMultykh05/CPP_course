a47ae0b05c385c4fce144a72de9bc379407f7b01
Multykh Oleksandr <alexmultykh2005@gmail.com>
2023-11-06 13:46:29 +0000

    Upload New File

---
+++ homeworks/homework 5/Queries.h
@@ -0,0 +1,13 @@
+#ifndef INC_5TH_HW_QUERIES_H
+#define INC_5TH_HW_QUERIES_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Movie.h"
+
+void db_query_1(const std::vector<Movie>& db, std::ostream& stream = std::cout);
+
+void db_query_2(const std::vector<Movie>& db, std::ostream& stream = std::cout);
+
+#endif //INC_5TH_HW_QUERIES_H
